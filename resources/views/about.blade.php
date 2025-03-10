bb59de3bcc1d7a9ba98cd900c6f5fa62c2e5f0cf
SanSiro2603 <143419496+SanSiro2603@users.noreply.github.com>
2025-03-10 11:48:47 +0800

    hai

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -25,7 +25,7 @@
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+    <span class="visually-hidden">Next nect</span>
   </button>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
